d9923e0a186e7679ff0fc8789fbeeb681a71f87f
Cabariqued27 <felipe27th@gmail.com>
2023-05-23 17:49:15 -0500

    navBar

--- app/src/components/navbar/NavBar.jsx
+++ app/src/components/navbar/NavBar.jsx
@@ -3,7 +3,15 @@ import "./navbar.css"
 
 function NavBar() {
   return (
-    <div>NavBar</div>
+    <nav>
+       <ul>
+        <li><a>Home</a></li>
+        <li><a>About</a></li>
+       <li><a>Services</a></li>
+       <li><a>Contact</a></li>
+      </ul>
+    </nav>
+
   )
 }
 
