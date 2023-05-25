0232aad04368a9bd8c9aae96315aaa238dd86c1f
Cabariqued27 <felipe27th@gmail.com>
2023-05-25 13:21:45 -0500

    navbar2.0

--- app/src/components/navbar/NavBar.jsx
+++ app/src/components/navbar/NavBar.jsx
@@ -1,18 +1,33 @@
-import React from 'react'
-import "./navbar.css"
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import HomeIcon from '@mui/icons-material/Home';
+import LoginIcon from '@mui/icons-material/Login';
+import PersonAddIcon from '@mui/icons-material/PersonAdd';
+export default function SimpleBottomNavigation() {
+  const [value, setValue] = React.useState(0);
 
-function NavBar() {
   return (
-    <nav>
-       <ul>
-        <li><a>Home</a></li>
-        <li><a>Login</a></li>
-       <li><a>Register</a></li>
-       <li><a>Contact</a></li>
-      </ul>
-    </nav>
-
-  )
+    <Box >
+      <BottomNavigation
+        showLabels
+        value={value}
+        onChange={(event, newValue) => {
+          setValue(newValue);
+        }}
+        sx={{
+          backgroundColor: '#e2e2e2',
+          '.MuiBottomNavigationAction-root.Mui-selected': {
+            color: '#088eff',
+          },
+        }}
+      >
+        <BottomNavigationAction label="Home" icon={<HomeIcon />} />
+        <BottomNavigationAction label="Register" icon={<PersonAddIcon />} />
+        <BottomNavigationAction label="Login" icon={<LoginIcon />} />
+        
+      </BottomNavigation>
+    </Box>
+  );
 }
-
-export default NavBar
