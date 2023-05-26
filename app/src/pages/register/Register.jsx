6b35fbf5ab31a7f24f7a95fa6973d36942e2a82e
Cabariqued27 <felipe27th@gmail.com>
2023-05-25 23:10:49 -0500

    Login/Register End 1.0

--- app/src/pages/register/Register.jsx
+++ app/src/pages/register/Register.jsx
@@ -12,13 +12,13 @@ export default function FormPropsTextFields() {
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
-        '& .MuiTextField-root': { m: 1, width: '25ch', display: 'block' },
+        '& .MuiTextField-root': { m: 1, width: '25ch', display: 'block',backgroundColor: '#F0F0F0' },
         '& .MuiButton-root': { m: 1,alignItems:'center' },'& h2': { fontFamily: 'Arial', fontWeight: 'normal' },
       }}
       noValidate
       autoComplete="off"
     >
-      <div style={{ borderRadius: '43px', border: '3px solid #e2e2e2', padding: '10px', textAlign: 'center',marginBottom:'10px' }}>
+      <div style={{  borderRadius: '13px',padding: '10px', textAlign: 'center',marginBottom:'10px', backgroundColor: '#e2e2e2 ' }}>
         <h2>Registro</h2>
         <TextField
           id="outlined-Name"
