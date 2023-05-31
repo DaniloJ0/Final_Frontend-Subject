8f81a4df238a5ca69709923a6902193d8e753047
Cabariqued27 <felipe27th@gmail.com>
2023-05-30 19:13:44 -0500

    Opiniones

--- app/src/components/contentHome/ContentHome.jsx
+++ app/src/components/contentHome/ContentHome.jsx
@@ -2,19 +2,87 @@ import React from 'react'
 
 function ContentHome() {
   return (
-    <>
-    <div className="h-screen flex justify-center content-center">
-        <div className="w-4/5">
-          Bienvenidooooo
-
-          Aqui solo informacion de relleno, trata que la pagina quede con estilo asi:
-
-          <a className='text-blue-700 font-bold' href="https://nicepage.com/es/st/4081915/reliability-quality-plantilla-web"> MIRA ESTE EJEMPLO, DA CLIC AQUI</a>
-
-          buscas imaganes claraas, asi como el primer parcial
+    
+    <div className="container my-24 px-6 mx-auto">
+      <section className="mb-32 text-gray-800 text-center">
+        <h2 className="text-3xl font-bold mb-12 pb-4 text-center">Opiniones de Nuestros Usuarios</h2>
+        <div className="grid lg:grid-cols-3 gap-6 xl:gap-x-12">
+          <div className="mb-6 lg:mb-0">
+            <div className="relative block bg-white rounded-lg shadow-lg">
+              <div className="flex">
+                <div className="relative overflow-hidden bg-no-repeat bg-cover relative overflow-hidden bg-no-repeat bg-cover shadow-lg rounded-lg mx-4 -mt-4"
+                  data-mdb-ripple="true" data-mdb-ripple-color="light">
+                  <img src="https://images.unsplash.com/photo-1532187863486-abf9dbad1b69?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80" className="w-full" alt="Article" />
+                  <a href="#!">
+                    <div className="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed opacity-0 hover:opacity-100 transition duration-300 ease-in-out"
+                      style={{ backgroundColor: 'rgba(251, 251, 251, 0.15)' }}></div>
+                  </a>
+                </div>
+              </div>
+              <div className="p-6">
+                <h5 className="font-bold text-lg mb-3">Facilidad</h5>
+                <p className="text-gray-500 mb-4">
+                  <small>Publicado <u>30.05.2023</u> Por 
+                    <a href="" className="text-gray-900"> Ana Castro</a></small>
+                </p>
+                <p className="mb-4 pb-2">
+                mi experiencia utilizando esta plataforma web para agendar mi cita médica fue excelente. Fue fácil de usar, la navegación fue intuitiva y logré agendar mi cita de manera rápida y eficiente. Sin duda, recomendaría esta plataforma a cualquiera que esté buscando un servicio de agendamiento de citas médicas confiable y conveniente
+                </p>
+                </div>
+            </div>
+          </div>
+    
+          <div className="mb-6 lg:mb-0">
+            <div className="relative block bg-white rounded-lg shadow-lg">
+              <div className="flex">
+                <div className="relative overflow-hidden bg-no-repeat bg-cover relative overflow-hidden bg-no-repeat bg-cover shadow-lg rounded-lg mx-4 -mt-4"
+                  data-mdb-ripple="true" data-mdb-ripple-color="light">
+                  <img src="https://plus.unsplash.com/premium_photo-1658506854326-52eab70cb236?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=869&q=80" className="w-full" alt="Article" />
+                  <a href="#!">
+                    <div className="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed opacity-0 hover:opacity-100 transition duration-300 ease-in-out"
+                      style={{ backgroundColor: 'rgba(251, 251, 251, 0.15)' }}></div>
+                  </a>
+                </div>
+              </div>
+              <div className="p-6">
+                <h5 className="font-bold text-lg mb-3">Intuitiva</h5>
+                <p className="text-gray-500 mb-4">
+                  <small>Publicado <u>20.05.2023</u> Por
+                    <a href="" className="text-gray-900"> Jhon Gomez</a></small>
+                </p>
+                <p className="mb-4 pb-2">Como usuario de un servicio web para agendar citas médicas, me complace decir que mi experiencia fue excelente. La plataforma web en la que me registré resultó ser muy intuitiva y fácil de usar. Desde el momento en que ingresé al sitio, pude navegar sin problemas y encontrar rápidamente la opción para agendar mi cita médica
+                </p>
+               </div>
+            </div>
+          </div>
+    
+          <div className="mb-0">
+            <div className="relative block bg-white rounded-lg shadow-lg">
+              <div className="flex">
+                <div className="relative overflow-hidden bg-no-repeat bg-cover relative overflow-hidden bg-no-repeat bg-cover shadow-lg rounded-lg mx-4 -mt-4"
+                  data-mdb-ripple="true" data-mdb-ripple-color="light">
+                  <img src="https://plus.unsplash.com/premium_photo-1663013467272-f0ca0edf1961?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80" className="w-full" alt="Article" />
+                  <a href="#!">
+                    <div className="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed opacity-0 hover:opacity-100 transition duration-300 ease-in-out"
+                      style={{ backgroundColor: 'rgba(251, 251, 251, 0.15)' }}></div>
+                  </a>
+                </div>
+              </div>
+              <div className="p-6">
+                <h5 className="font-bold text-lg mb-3">Solución</h5>
+                <p className="text-gray-500 mb-4">
+                  <small>Publicado <u>20.05.2023</u> Por
+                    <a href="" className="text-gray-900"> Sandra Arrieta</a></small>
+                </p>
+                <p className="mb-4 pb-2">
+                Recientemente, tuve la oportunidad de utilizar una plataforma en línea para agendar una cita médica, y debo decir que mi experiencia fue excelente. Anteriormente, solía pasar horas esperando en filas para obtener una cita médica, pero gracias a este servicio, pude evitar todo ese tiempo de espera y hacerlo de manera rápida y conveniente.
+                </p>
+                </div>
+            </div>
+          </div>
         </div>
+      </section>
     </div>
-    </>
     
   )
 }
