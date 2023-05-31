e687ab3f8b4a992a6bb4573317083011be2ef0a2
Cabariqued27 <felipe27th@gmail.com>
2023-05-30 19:53:04 -0500

    About us Lorem

--- app/src/components/contentHome/ContentHome.jsx
+++ app/src/components/contentHome/ContentHome.jsx
@@ -3,6 +3,76 @@ import React from 'react'
 function ContentHome() {
   return (
     
+<> <div className="container my-24 px-6 mx-auto">
+    <section className="mb-32 text-gray-800">
+      <div className="container mx-auto xl:px-32 text-center lg:text-left">
+        <div className="grid lg:grid-cols-2 flex items-center">
+          <div className="mb-12 lg:mb-0">
+            <div
+              className="block rounded-lg shadow-lg px-6 py-12 lg:py-6 xl:py-12 md:px-12 lg:-mr-14"
+              style={{ background: 'hsla(0, 0%, 100%, 0.55)', backdropFilter: 'blur(30px)' }}
+            >
+              <h3 className="text-2xl font-bold mb-3">We know how valuable your time is</h3>
+              <h5 className="text-lg text-blue-600 font-bold mb-12 lg:mb-10 xl:mb-12">
+                Let us answer your questions
+              </h5>
+
+              <p className="font-bold mb-4">Anim pariatur cliche reprehenderit?</p>
+              <p className="text-gray-500 mb-6">
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt autem numquam dolore
+                molestias aperiam culpa alias veritatis architecto eos, molestiae vitae ex eligendi
+                libero eveniet dolorem, doloremque rem aliquid perferendis.
+              </p>
+
+              <p className="font-bold mb-4">Non cupidatat skateboard dolor brunch?</p>
+              <p className="text-gray-500 mb-6">
+                Distinctio corporis, iure facere ducimus quos consectetur ipsa ut magnam autem
+                doloremque ex! Id, sequi. Voluptatum magnam sed fugit iusto minus et suscipit?
+                Minima sunt at nulla tenetur, numquam unde quod modi magnam ab deserunt ipsam sint
+                aliquid dolores libero repellendus cupiditate mollitia quidem dolorem odit
+              </p>
+
+              <p className="font-bold mb-4">Praesentium voluptatibus temporibus consequatur non aspernatur?</p>
+              <p className="text-gray-500">
+                Minima sunt at nulla tenetur, numquam unde quod modi magnam ab deserunt ipsam sint
+                aliquid dolores libero repellendus cupiditate mollitia quidem dolorem.
+              </p>
+            </div>
+          </div>
+
+          <div>
+            <img
+              src="https://images.unsplash.com/photo-1583912267856-1fcdf6e0a1f9?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=387&q=80"
+              className="w-full rounded-lg shadow-lg"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     <div className="container my-24 px-6 mx-auto">
       <section className="mb-32 text-gray-800 text-center">
         <h2 className="text-3xl font-bold mb-12 pb-4 text-center">Opiniones de Nuestros Usuarios</h2>
@@ -83,6 +153,8 @@ function ContentHome() {
         </div>
       </section>
     </div>
+    
+</>
     
   )
 }
