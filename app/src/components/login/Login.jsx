824063a63662ee5968fdb5b3ef488c324ea4434e
Cabariqued27 <felipe27th@gmail.com>
2023-06-01 12:02:38 -0500

    new login/register old work

--- app/src/components/login/Login.jsx
+++ app/src/components/login/Login.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import SendIcon from "@mui/icons-material/Send";
+import Button from "@mui/material/Button";
 
 function Login() {
   const [formData, setFormData] = useState({
@@ -49,66 +52,59 @@ function Login() {
   };
 
   return (
-      <Box onSubmit={handleSubmit}
+    <div className="h-screen mt-10">
+      <Box
+        component="form"
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          "& .MuiTextField-root": {
+            m: 1,
+            width: "25ch",
+            display: "block",
+            backgroundColor: "#F0F0F0",
+          },
+          "& .MuiButton-root": { m: 1, alignItems: "center" },
+          "& h2": { fontFamily: "Arial", fontWeight: "normal" },
+        }}
+        onSubmit={handleSubmit}
         noValidate
-        autoComplete="off">
-      <div className="min-h-screen  py-6 flex flex-col justify-center sm:py-12">
-      <div className="relative py-3 sm:max-w-xl sm:mx-auto">
-        <div className="absolute inset-0 bg-gradient-to-r from-blue-300 to-blue-600 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
-        <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
-          <div className="max-w-md mx-auto">
-            <div>
-              <h1 className="text-2xl font-semibold">Ingresa</h1>
-            </div>
-            <div className="divide-y divide-gray-200">
-              <div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
-                <div className="relative">
-                  <input
-                    autoComplete="off"
-                    id="email"
-                    name="email"
-                    type="text"
-                    value={formData.email}
-                    onChange={handleInputChange}
-                    className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:border-rose-600"
-                    placeholder="Email address"
-                  />
-                  <label
-                    htmlFor="email"
-                    className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
-                  >
-                    Email Address
-                  </label>
-                </div>
-                <div className="relative">
-                  <input
-                    autoComplete="off"
-                    id="password"
-                    name="password"
-                    type="password"
-                    value={formData.password}
-                    onChange={handleInputChange}
-                    className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:border-rose-600"
-                    placeholder="Password"
-                  />
-                  <label
-                    htmlFor="password"
-                    className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
-                  >
-                    Password
-                  </label>
-                </div>
-                <div className="relative">
-                  <button className="bg-blue-500 text-white rounded-md px-2 py-1">Submit</button>
-                </div>
-              </div>
-            </div>
-          </div>
+        autoComplete="off"
+      >
+        <div
+          style={{
+            borderRadius: "13px",
+            padding: "10px",
+            textAlign: "center",
+            backgroundColor: "#e2e2e2 ",
+          }}
+        >
+          <h2>Login</h2>
+          <TextField
+            id="outlined-required"
+            name="email"
+            value={formData.email}
+            onChange={handleInputChange}
+            label="Email"
+            type="email"
+          />
+          <TextField
+            id="outlined-password-input"
+            name="password"
+            label="Password"
+            type="password"
+            autoComplete="current-password"
+            value={formData.password}
+            onChange={handleInputChange}
+          />
+          <Button type="submit" variant="contained" endIcon={<SendIcon />}>
+            Send
+          </Button>
         </div>
-      </div>
-    </div>
-    {errorMessage && <p>{errorMessage}</p>}
+        {errorMessage && <p sx={{ color: "red" }}>{errorMessage}</p>}
       </Box>
+    </div>
   );
 }
 
